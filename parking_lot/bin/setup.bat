a19d31653a78451919f3677af85d2756b45665a6
Miftahul Huda <wiseape.technologies@gmail.com>
2019-03-17 18:35:26 +0700

    Setup and Run program

--- parking_lot/bin/setup.bat
+++ parking_lot/bin/setup.bat
@@ -1,4 +1,12 @@
-..\gradlew.bat
-cd main
+@echo OFF
+cd ..
+call gradlew.bat compileJava
+xcopy /E /Y build\classes\java\main  bin\main
+cd bin\main
+echo  ----
+echo  ----
+echo -----  
+echo ================TESTING==============
+
 java -cp packages/junit-4.13-beta-2.jar;packages/hamcrest-all-1.3.jar;. com.huda.unittesting.TestRunner
 cd ..
